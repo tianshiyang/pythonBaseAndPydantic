be034361801b032c37eefed2803b8b7444e511ef
田世洋 <tianshiyang@icourt.cc>
2025-09-27 10:29:14 +0800

    初始化项目

---
+++ pydantic_study/demo1.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 27.9.25 AM10:16
+@Author  : tianshiyang
+@File    : demo1.py.py
+"""
+
+def test_assert(x):
+    assert x == 5
+    print("ok")
+
+def test():
+    test_assert(10)
